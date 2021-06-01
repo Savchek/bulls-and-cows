1ee682e3714a17e94af6b33f58c59491deab252a
Savchek <32517761+Savchek@users.noreply.github.com>
2021-06-01 21:07:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let data = [], plan=[], sNum = []
-let greenC = '#64DF85',
-	redC = '#FF8373'
-let game = false,
-	planning = false,
-	turn = 1
-let pl = document.getElementById('play'),
-	tr = document.getElementById('turns'),
-	sr = document.getElementById('surrender')
+let data = []
+let plan=[]
+let sNum = []
+
+let greenC = '#64DF85'
+let redC = '#FF8373'
+
+let game = false
+let planning = false
+let turn = 1
+
+let pl = document.getElementById('play')
+let tr = document.getElementById('turns')
+let	sr = document.getElementById('surrender')
 
 buildAnswers = () => {
     for (var a = [], d = 0; 10 > d; ++d)
@@ -15,10 +20,12 @@ buildAnswers = () => {
                 for (var c = 0; 10 > c; ++c)
                     if (c != b && c != d)
                         for (var e = 0; 10 > e; ++e) e != c && e != b && e != d && a.push([d, b, c, e])
-    return a
+    
+	return a
 }
 
 let arr = buildAnswers(), z = [1, 2, 3, 4, 5, 6, 7, 8, 9].sort(() => Math.random() - .5)
+
 z.push(z[Math.floor(Math.random()*4)], z[Math.floor(Math.random()*4)+4], 0)
 
 start = () => {
@@ -26,48 +33,55 @@ start = () => {
 		let s = Math.floor(Math.random() * 10)
 		!(sNum.indexOf(s) != -1) ? sNum.push(s) : i--
 	}
+	
 	pl.style.marginTop = '-130px'
 	setTimeout(() => pl.style.display = 'none', 200)
 	game = true
 	wrt('Придумайте та введіть ваше число')
-	document.getElementById('right').scrollTop = -999
+	document.getElementById('left').scrollTop = -999
 	planning = true
 }
 
 reset = (lose) => {
 	tr.style.marginTop = '-130px'
+
 	setTimeout(() => {
 		tr.style.display = sr.style.display = 'none'
 		pl.style.display = 'block'
 		pl.style.marginTop = '0'
 		tr.innerHTML = 'Хід #1'
 	}, 200)
+
 	game = false
 	turn = 1
+
 	if (lose){
-		wrt('Ви здалися😢 Число суперника: '+sNum.join(''))
+		wrt('Ви здалися😢 Число суперника: ' + sNum.join(''))
 		document.getElementById('lastp').style.backgroundColor = redC
 	}
+
 	for (let i = 0; i < 4; i++){
 		document.getElementById('d'+i).innerHTML = ''
 	}
+
 	arr = buildAnswers(), z = [1, 2, 3, 4, 5, 6, 7, 8, 9].sort(() => Math.random() - .5)
-	z.push(z[Math.floor(Math.random()*4)], z[Math.floor(Math.random()*4)+4], 0)
+	z.push(z[Math.floor(Math.random() * 4)], z[Math.floor(Math.random() * 4) + 4], 0)
 	data = []
 	sNum = []
 }
 
-blink = (el, color, regularColor, time) => {
+blink = (el, color, regularColor = '#C5FFFF', time = 200) => {
 	el.style.background = color
-	setTimeout(() => el.style.background = regularColor || '#C5FFFF', time || 200)
+	setTimeout(() => el.style.background = regularColor, time)
 }
 
 blinkTopNum = (color, onlyEmpty) => {
 	for (let i = 0; i < 4; i++){
-		let e = document.getElementById('d'+i)
+		let e = document.getElementById('d' + i)
+
 		if (!onlyEmpty){
 			blink(e, color)
-		} else if (e.innerHTML==''){
+		} else if (e.innerHTML == ''){
 			blink(e, color)
 		}
 	}
@@ -79,37 +93,42 @@ addNum = num => {
 
 		if (num>=0){
 			for (let i = 0; i < 4; i++){
-				let el = document.getElementById('d'+i)
+				let el = document.getElementById('d' + i)
 
-				if(!data[i] && !(data.indexOf(num) != -1) && data[i]!=0){
+				if(!data[i] && !(data.indexOf(num) != -1) && data[i] != 0) {
 					data[i] = el.innerHTML = num
 					blink(el, greenC)
+
 					break
-				}else if (len == 4){
+				} else if(len == 4) {
 					blink(document.getElementById('go'), greenC)
 					blinkTopNum(greenC)
+
 					break
-				}else if(el.innerHTML == num && el.innerHTML!=''){
+				} else if(el.innerHTML == num && el.innerHTML!='') {
 					blink(el, redC)
 					blink(document.getElementById('np'+num), redC)
 				}
 			}
-		}else if(num===-1){
-			if (len){
+		} else if(num === -1) {
+			if (len) {
 				let deletedElement = document.getElementById('d' + data.indexOf( data[len-1] ) )
+
 				deletedElement.innerHTML = ''
 				blink(deletedElement, 'white')
 				data.pop()
-			}else{
+			} else {
 				blinkTopNum(redC)
 				blink(document.getElementById('del'), redC)
 			}
-		}else if(num===-2){
-			if (len<4){
+		} else if(num === -2) {
+			if (len < 4) {
 				blinkTopNum(redC, true)
 				blink(document.getElementById('go'), redC)
+
 				return
 			}
+
 			if (planning){
 				plan = [...data]
 				data = []
@@ -118,12 +137,14 @@ addNum = num => {
 				tr.style.marginTop = '-130px'
 				setTimeout(() => tr.style.marginTop = '0', 200)
 
-				for (let i = 0; i < 4; i++)
+				for (let i = 0; i < 4; i++) {
 					document.getElementById('d'+i).innerHTML = ''
+				}
+
 				wrt('Суперник також загадав число')
-				wrt('Гра почалася🏁')
+				wrt('🚩Гра почалася🚩')
 				blinkTopNum(greenC)
-			}else{
+			} else {
 				compare()
 			}
 		}
@@ -134,74 +155,90 @@ addNum = num => {
 
 compare = () => {
 	let playerres = getBullsCows(data, sNum)
-	let bulls = playerres.bulls,
-		cows = playerres.cows
+	let bulls = playerres.bulls
+	let	cows = playerres.cows
 		
-	tr.innerHTML = 'Хід #'+(++turn)
-	if(bulls == 4){
-		wrt('Ви виграли🎉 Число: ' + sNum.join('') + ', Хід #' + (--turn))
+	tr.innerHTML = 'Хід #' + (++turn)
+
+	if(bulls == 4) {
+		wrt('Ви виграли🎉 Число суперника: ' + sNum.join('') + ', Хід #' + (--turn))
 		document.getElementById('lastp').style.backgroundColor = greenC
 		reset()
 		blinkTopNum(greenC)
-	} else if (bulls>=0 && bulls<4 && cows>=0 && cows<=4){
+	} else if (bulls >= 0 && bulls < 4 && cows >= 0 && cows <= 4) {
 		let botRes = choseAnswer()
+
 		if (botRes.bulls == 4){
 			wrt('🤖Cуперник виграв! Ваше число: ' + botRes.num.join('') + ', Число суперника: ' + sNum.join('') + ', Хід #' + (--turn))
 			document.getElementById('lastp').style.backgroundColor = redC
 			reset()
-		}else{
+		} else {
 			let bguessed = getVisGuess(botRes.bulls, botRes.cows)
 			let pguessed = getVisGuess(playerres.bulls, playerres.cows)
-			wrt('🤖Спроба суперника: У числі ' + botRes.num.join('') + bguessed)
-			wrt('🤔️Ваша спроба: У числі ' + data.join('') + pguessed)
+
+			wrt('🤖Спроба суперника: ' + botRes.num.join('') + ' ' + bguessed)
+			wrt('🤔️Ваша спроба: ' + data.join('') + ' ' + pguessed)
 		}
-		data=[]
+
+		data = []
 	}
 }
 
 getVisGuess = (bulls, cows) => {
-	let guessed = '[',
-		fb = fc = 1
+	let guessed = '['
+	let fb = 1
+	let fc = 1
 
 	for (let i = 0; i < 4; i++){
-		document.getElementById('d'+i).innerHTML = ''
+		document.getElementById('d' + i).innerHTML = ''
 	}
+
 	for (let b = 0; b < 4; b++){
-		if(bulls>0){
-			guessed+='🐂'
-			fb=0
+		if(bulls > 0){
+			guessed += '🐂'
+			fb = 0
 		}
+
 		bulls--
 	}
-	guessed+=fb?'➖][':']['
-	for (let c = 0; c < 4; c++){
-		if(cows>0){
-			guessed+='🐄'
-			fc=0
+
+	guessed += fb ? '➖][' : ']['
+
+	for (let c = 0; c < 4; c++) {
+		if(cows > 0){
+			guessed += '🐄'
+			fc = 0
 		}
+
 		cows--
 	}
-	guessed+=fc?'➖]':']'
+
+	guessed += fc ? '➖]' : ']'
+
 	return guessed
 }
 
 filterAnswers = (a, d) => {
-    for (var b = a.length - 1; - 1 < b; b--) d(a[b]) || a.splice(b, 1)
+    for (let b = a.length - 1; -1 < b; b--) {
+		d(a[b]) || a.splice(b, 1)
+	}
 }
 
 choseAnswer = () => {
 	let num = getNum(arr)
 	let resBK = getBullsCows(num, plan)
-	let bulls = resBK.bulls,
-		cows = resBK.cows
+	let bulls = resBK.bulls
+	let cows = resBK.cows
+
 	filter = test(num, bulls, cows)
 	filterAnswers(arr, filter)
-	console.log(arr.length)
-	return res = {num, bulls, cows}
+	
+	return {num, bulls, cows}
 }
 
 getBullsCows = (att, com) => {
-	let bulls = 0, cows = 0
+	let bulls = 0
+	let cows = 0
 
 	att.forEach((attVal, attInd) => {
 		com.forEach((comVal, comInd) => {
@@ -212,7 +249,8 @@ getBullsCows = (att, com) => {
 			}
 		})
 	})
-	return res={bulls, cows}
+
+	return {bulls, cows}
 }
 
 test = (a, d, b) => {
@@ -225,16 +263,21 @@ test = (a, d, b) => {
 getNum = a => z.length && a.length > 10 ? z.splice(0, 4) : a[Math.floor(Math.random() * a.length)]
 
 wrt = text => {
-	let a = document.getElementById('right')
+	let a = document.getElementById('left')
 	let b = document.getElementById('lastp')
 	let neq = document.createElement('p')
+
 	neq.style.marginTop = '-104px'
-	
 	neq.innerHTML = text
 	a.insertBefore(neq, b)
-
 	b.removeAttribute('id')
 	neq.setAttribute('id', 'lastp')
 	setTimeout(()=>neq.style.marginTop = 0,100)
-	document.getElementById('right').scrollTop = -999
+	document.getElementById('left').scrollTop = -999
+}
+
+info = () => {
+	wrt('<a target="_blank" href="https://github.com/Savchek/bulls-and-cows/blob/master/LICENSE">Ліцензія MIT</a>')
+	wrt('Розробив студент Університету імені Івана Франка Савченко Данило')
+	wrt('Загальна інформація: ')
 }
